6f81688edd5b300890c75adc8989c6f58655d856
Tomi Germanow <tomi.germanow@aktia.fi>
2019-06-18 16:17:52 +0300

    Added click

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ aws configure --profile python-course
 pip3 install pipenv
 pipenv --three
 pipenv install boto3
+pipenv install click
 pipenv install -d ipython
 pipenv run ipython
 ```
--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,9 +1,24 @@
 import boto3
+import click
+
+session = boto3.Session(profile_name='python-course')
+ec2 = session.resource('ec2')
+
+@click.command() #@ is decorator/wrapper
+def list_instances():
+    "List all EC2 intances"
+    for i in ec2.instances.all():
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+
+    return
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='python-course')
-    ec2 = session.resource('ec2')
-    
-    for i in ec2.instances.all():
-        print(i)
+    list_instances()
+
+
 
