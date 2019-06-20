2890af4e2164f1a2dcb9a8850ea459060309abbb
Tomi Germanow <tomi.germanow@aktia.fi>
2019-06-20 10:21:35 +0300

    Added snapshoting

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -92,7 +92,32 @@ def stop_instances(owner):
     
     return
     
+@instances.command("snapshot")
+@click.option('--owner', default=None, help="Only instances for the Owner")
+def create_snapshots(owner):
+    """Create snapshots for EC2 instances"""
     
+    instances = filter_instances(owner)
+    
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        
+        i.stop()
+        i.wait_until_stopped()
+        
+        for v in i.volumes.all():
+            print("Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by acg-python-course script")
+        
+        print("Starting {0}...".format(i.id))
+        i.start()
+        i.wait_until_running()
+        
+    
+    print("Snapshots done.")
+    
+    return
+
     
 if __name__ == '__main__':
     cli()
